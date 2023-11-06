502d96f12f1f659646252b483505bd6ec95141d0
justCongDoan <congdoan2002tb2@gmail.com>
2023-11-06 21:33:18 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Testing from './src/screens/Testing';
+
+const App = () => {
+  return (
+    <Testing/>
+  );
+};
+
+export default App;
---
+++ src/screens/Testing.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+
+const Testing = () => {
+  return (
+    <View>
+      <FontAwesome5 name='bolt' size={20} color='gray'/>
+    </View>
+  )
+}
+
+export default Testing
+
+const styles = StyleSheet.create({})
