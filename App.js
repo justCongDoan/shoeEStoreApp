314c191c457461762a4748abc51af52c0a3e7d52
justCongDoan <congdoan2002tb2@gmail.com>
2023-11-06 21:59:00 +0700

    initialized Product deatails screen

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import ProductScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <ProductScreen/>
+      {/* <ProductScreen/> */}
+      <ProductDetailsScreen/>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+})
